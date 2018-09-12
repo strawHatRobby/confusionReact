8a51ca7cbb0b7ba991e99e6d24207ba3c037c543
robs100DaysOfCode <robfr93@icloud.com>
2018-09-12 19:37:38 +0530

    "implemented a simple login modal form that does nothing"

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -11,7 +11,9 @@ import {
   Row,
   Col,
   Jumbotron,
-  Button
+  Button,
+  Form, FormGroup, Input, Label,
+  Modal, ModalHeader, ModalBody
 } from 'reactstrap';
 
 
@@ -19,10 +21,27 @@ class Header extends Component {
   constructor(props){
     super(props);
     this.state = {
-        isNavOpen: false
+        isNavOpen: false,
+        isModalOpen: false
 
     };
     this.toggleNav = this.toggleNav.bind(this);
+    this.toggleModal = this.toggleModal.bind(this);
+    this.handleLogin = this.handleLogin.bind(this);
+  }
+
+  handleLogin(event) {
+    this.toggleModal();
+    console.log("username " + this.username.value + " has password " + this.password.value);
+    alert("username " + this.username.value + " has password " + this.password.value);
+    event.preventDefault();
+  }
+
+  toggleModal() {
+    this.setState({
+      isModalOpen: !this.state.isModalOpen
+    });
+
   }
 
   toggleNav() {
@@ -58,13 +77,21 @@ class Header extends Component {
             </NavItem>
             <NavItem>
               <NavLink className="nav-link" to="/menu">
-                <span className="fa fa-list fa-lg"></span> Menu
+                <span className="fa fa-list fa-lg"></span>
+                            Menu
               </NavLink>
             </NavItem>
             <NavItem>
               <NavLink className="nav-link" to="/contactus">
-                <span className="fa fa-address-card fa-lg"></span> Contact
+                <span className="fa fa-address-card fa-lg"></span>
+                          Contact
               </NavLink>
+            </NavItem>
+          </Nav>
+          <Nav className="ml-auto" navbar>
+            <NavItem className="nav-link" onClick={this.toggleModal}>
+                <span className="fa fa-sign-in fa-lg"></span>
+                  Login
             </NavItem>
           </Nav>
         </Collapse>
@@ -82,6 +109,40 @@ class Header extends Component {
           </div>
         </div>
       </Jumbotron>
+      <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModa}>
+        <ModalHeader toggle={this.toggleModal}>Login</ModalHeader>
+        <ModalBody>
+            <Form onSubmit={this.handleLogin}>
+              <FormGroup row>
+              <Col md={10}>
+                <Label htmlFor="username">username</Label>
+                <Input type="text" id="username" name="username"
+                  innerRef={(input) => this.username = input}
+                />
+                </Col>
+              </FormGroup>
+              <FormGroup row>
+              <Col md={10}>
+                <Label htmlFor="password">password</Label>
+                <Input type="password" id="password" name="password"
+                innerRef={(input) => this.password = input}
+                 />
+                </Col>
+              </FormGroup>
+              <FormGroup check>
+              <Col md={10}>
+              <Input type="checkbox" id="remember_me" name="remember_me"
+              innerRef={(input) => this.remember_me = input}
+              />
+                <Label htmlFor="remember_me">remember me</Label>
+                </Col>
+              </FormGroup>
+              <Button type="submit" value="submit" color="primary">
+              Login
+              </Button>
+            </Form>
+        </ModalBody>
+      </Modal>
       </React.Fragment>
     )
   }
