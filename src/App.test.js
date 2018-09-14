45fc3e47c3b187ce286333ab6dd3cd19f4d085a8
--global <--global>
2018-09-14 13:41:45 +0530

    "added router and store to the App.test.js, only checks whether hom page renders or not"

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
+import { ConfigureStore } from './redux/configureStore';
+
+const store = ConfigureStore();
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(
+    <Provider store={store}>
+
+      <BrowserRouter>
+          <App />
+      </BrowserRouter>
+    </Provider>
+
+     ,div);
   ReactDOM.unmountComponentAtNode(div);
 });
